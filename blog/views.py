7578332791fcfaee82019f3313ec56cec7629bc3
Kzt Tateyama <kzt.tateyama@gmail.com>
2019-04-08 22:47:49 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
+""" カレントディレクトリにある「model.py」に定義している Postオブジェクトを使う"""
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    """ 「published_date」の昇順にリストを並べ替える。"""
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
