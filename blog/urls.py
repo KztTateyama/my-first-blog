950c41da9603c2cfb7adcdeb58442fcb1f18d41a
Kzt Tateyama <kzt.tateyama@gmail.com>
2019-04-15 23:12:00 +0900

    Changed the HTML for the site. 20190415 231100

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 """post_listという名前のviewをルートURLに割り当てる。"""
 """Webサイトにアクセスしてきた最初の人はここに誘導される。"""
+"""int:pk 整数の値を期待し、その値がpkという名前の変数でビューに渡されることを意味しています。"""
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 from .models import Post
 """ カレントディレクトリにある「model.py」に定義している Postオブジェクトを使う"""
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -16,3 +16,6 @@ def post_list(request):
          {'posts': posts} :  渡す情報。ここの情報がhtmlに渡される。
     """
 
+def post_detail(reqest, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(reqest, 'blog/post_detail.html', {'post':post})
